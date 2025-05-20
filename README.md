57cff6fdd98b6f742c31281defaa3cbb13d7d30a
bisnap <194406514+bisnapYT@users.noreply.github.com>
2025-05-20 12:37:26 -0400

    v1.1 Update

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
 # Borderlands 2 Anarchy Saver
-- Saves a customizable proportion of your Anarchy stacks between save quits
-- Adds keybinds to add and remove Anarchy
-- Allows for uncapping max Anarchy
+
+## Features
+- Saves a customizable percentage of your Anarchy stacks between save quits
+- Allows you to remove the Anarchy cap
+- Adds keybinds to add Anarchy, subtract Anarchy, and display current Anarchy (useful when over 999 stacks as the game does not display more than 3 digits with the exception of thin numbers like 1011)
+
+
+## Version History
+- 1.1 (current)
+    - Adjusted user-facing descriptions
+    - Added 'Display Current Anarchy' keybind 
+    - Added 'Display Anarchy Past 999' option
+    - Changing the 'Uncap Anarchy' option no longer requires a save quit to apply
+    - Added the `set_anarchy` function. If you want to use it instead of the keybinds, in the Borderlands 2 console use `py from AnarchySaver import set_anarchy` and then `py set_anarchy(desired_stack_count)` (the function must be imported again every time you launch the game)
+- 1.0.1
+    - Adjusted user-facing descriptions
+    - Fixed the .sdkmod file
+- 1.0
+    - Initial release
+
+
+## Known Issues
+- Due to floating point inaccuracy, Anarchy will not naturally stack higher than 16,777,216. To bypass this, you must use the 'Add Anarchy' keybind or import and use the `set_anarchy` function. This is the game's issue and not mine. Fixing this is possible, but adjusting the increment of stack gain and loss is a lot of work in determining each of which of the 24 Behavior_SimpleMath calls in the Anarchy BehaviorProviderDefinition correspond to gain or loss of stacks, as well as if Typecast Iconoclast is handled separately. I know some, but checking each and every one in several situations is time consuming for an issue that is both circumventable using the tools I've provided and unlikely to be experienced by anybody in the first place. 29,360,128% damage up is probably enough
+- Discord doesn't actually drain your Anarchy if you activate it with 750 or more stacks. This is the game's issue and not mine
--- __init__.py
+++ __init__.py
@@ -1,28 +1,92 @@
 import mods_base
 from mods_base import get_pc, SliderOption, keybind, BoolOption
-from unrealsdk import find_object
+from unrealsdk import find_object, hooks, unreal
+from ui_utils import show_hud_message
+from math import inf
+from typing import Any
 from save_options.options import HiddenSaveOption
 from save_options.registration import register_save_options
-
 
 def get_anarchy_attrs():
     try:
         return (
             find_object('DesignerAttributeDefinition', 'GD_Tulip_Mechromancer_Skills.Misc.Att_Anarchy_NumberOfStacks'),
-            find_object('DesignerAttributeDefinition', 'GD_Tulip_Mechromancer_Skills.Misc.Att_Anarchy_StackCap')
+            find_object('DesignerAttributeDefinition', 'GD_Tulip_Mechromancer_Skills.Misc.Att_Anarchy_NumberOfStacks').GetValue(get_pc())[0],
+            find_object('DesignerAttributeDefinition', 'GD_Tulip_Mechromancer_Skills.Misc.Att_Anarchy_StackCap'),
+            find_object('DesignerAttributeDefinition', 'GD_Tulip_Mechromancer_Skills.Misc.Att_Anarchy_StackCap').GetValue(get_pc())[0]
         )
     except ValueError:
-        return None, None
+        return None, None, None, None
+
+def set_anarchy(stack: int|float):
+    find_object('DesignerAttributeDefinition', 'GD_Tulip_Mechromancer_Skills.Misc.Att_Anarchy_NumberOfStacks').SetAttributeBaseValue(get_pc(), stack)
+
+def uncap_anarchy():
+    *_, stack_cap_attr, _ = get_anarchy_attrs()
+    if stack_cap_attr:
+        stack_cap_attr.SetAttributeBaseValue(get_pc(), inf)
+
+def cap_anarchy():
+    _, num_stacks, stack_cap_attr, stack_cap = get_anarchy_attrs()
+    if stack_cap_attr and num_stacks is not None and stack_cap is not None:
+        stack_cap_attr.SetAttributeBaseValue(get_pc(), 0)
+        stack_cap = find_object('DesignerAttributeDefinition', 'GD_Tulip_Mechromancer_Skills.Misc.Att_Anarchy_StackCap').GetValue(get_pc())[0]
+        if num_stacks > stack_cap:
+                set_anarchy(stack_cap)
+
+@mods_base.hook(
+        "WillowGame.Behavior_SimpleMath:ApplyBehaviorToContext",
+        hook_type=hooks.Type.POST
+)
+def simple_math(
+    caller: unreal.UObject,
+    args: unreal.WrappedStruct,
+    return_value: Any,
+    function: unreal.BoundFunction
+) -> None:
+    if not caller._path_name().startswith("GD_Tulip_Mechromancer_Skills.EmbraceChaos.Anarchy:BehaviorProviderDefinition_0"):
+        return
+    num_stacks = int(find_object('DesignerAttributeDefinition', 'GD_Tulip_Mechromancer_Skills.Misc.Att_Anarchy_NumberOfStacks').GetValue(get_pc())[0])
+    if num_stacks == 999:
+        get_pc().GetHUDMovie().ClearTrainingText()
+    if num_stacks > 999:
+        get_pc().GetHUDMovie().AddTrainingText(
+            MessageString = f"{num_stacks:,}",
+            TitleString = "Current Anarchy",
+            Duration = inf,
+            DrawColor = unreal.IGNORE_STRUCT,
+            HUDInitializationFrame = "",
+            PausesGame = False,
+            PauseContinueDelay = 0,
+            Related_PRI1 = get_pc().PlayerReplicationInfo,
+            bIsntActuallyATrainingMessage = True
+        )
+
+def toggle_anarchy_cap(option: BoolOption, value: bool):
+    if value:
+        uncap_anarchy()
+    else:
+        cap_anarchy()
+
+def toggle_anarchy_display(option: BoolOption, value: bool):
+    if value:
+        simple_math.enable()
+    else:
+        simple_math.disable()
 
 stack_uncap = BoolOption(
     identifier="Uncap Anarchy Stacks",
     value=False,
-    description="'On' sets Anarchy cap to inf whenever loading from the main menu. 'Off' allows the game to sanity check based on your skill tree and will apply after the next load from the main menu."
+    description="'On' sets your Anarchy cap to infinity. 'Off' allows the game to set your Anarchy cap as normal.",
+    on_change=toggle_anarchy_cap
 )
-def uncap_anarchy():
-    _, stack_cap_attr = get_anarchy_attrs()
-    if stack_cap_attr:
-        stack_cap_attr.SetAttributeBaseValue(get_pc(), float('inf'))
+
+display_anarchy = BoolOption(
+    identifier="Display Anarchy Past 999",
+    value=False,
+    description="As long as you have more than 999 Anarchy, a persistent HUD message shows your current stack count.",
+    on_change=toggle_anarchy_display
+)
 
 anarchy_multiplier = SliderOption(
     identifier="Percentage of Anarchy to restore",
@@ -30,7 +94,7 @@ anarchy_multiplier = SliderOption(
     is_integer=True,
     min_value=0,
     max_value=100,
-    description="Multiplies your saved anarchy value by specified percentage when loading the game."
+    description="Multiplies your saved Anarchy value by the specified percentage when loading the game."
 )
 
 anarchy_keys = SliderOption(
@@ -39,61 +103,72 @@ anarchy_keys = SliderOption(
     is_integer=True,
     min_value=1,
     max_value=600,
-    description="How much Anarchy is added or subtracted when activating the corresponding keybinds. Will not add Anarchy above your cap or subtract to below 0."
+    description="How much Anarchy is added or subtracted when using the corresponding keybinds. Will not add Anarchy above your cap or subtract to below 0."
 )
 def get_anarchy_key_value():
     return anarchy_keys.value
 
 @keybind("Add Anarchy")
 def add_anarchy_key_pressed():
-    num_stacks_attr, stack_cap_attr = get_anarchy_attrs()
-    if num_stacks_attr and stack_cap_attr:
-        stacks, *_ = num_stacks_attr.GetValue(get_pc())
-        if (stacks + get_anarchy_key_value()) > stack_cap_attr.GetValue(get_pc())[0]:
-            num_stacks_attr.SetAttributeBaseValue(get_pc(), stack_cap_attr.GetValue(get_pc())[0])
+    num_stacks_attr, num_stacks, stack_cap_attr, stack_cap = get_anarchy_attrs()
+    if num_stacks_attr and num_stacks is not None and stack_cap_attr and stack_cap is not None:
+        if (num_stacks + get_anarchy_key_value()) > stack_cap:
+            set_anarchy(stack_cap)
         else:
-            num_stacks_attr.SetAttributeBaseValue(get_pc(), stacks + get_anarchy_key_value())
+            set_anarchy(num_stacks + get_anarchy_key_value())
 
 @keybind("Subtract Anarchy")
 def subtract_anarchy_key_pressed():
-    num_stacks_attr, *_ = get_anarchy_attrs()
-    if num_stacks_attr:
-        stacks, *_ = num_stacks_attr.GetValue(get_pc())
-        if (stacks - get_anarchy_key_value()) < 0:
-            num_stacks_attr.SetAttributeBaseValue(get_pc(), 0)
+    num_stacks_attr, num_stacks, *_ = get_anarchy_attrs()
+    if num_stacks_attr and num_stacks is not None:
+        if (num_stacks - get_anarchy_key_value()) < 0:
+            set_anarchy(0)
         else:
-            num_stacks_attr.SetAttributeBaseValue(get_pc(), stacks - get_anarchy_key_value())
+            set_anarchy(num_stacks - get_anarchy_key_value())
+
+@keybind("Display Current Anarchy")
+def display_anarchy_key_pressed():
+    num_stacks_attr, num_stacks, *_ = get_anarchy_attrs()
+    if num_stacks_attr and num_stacks is not None:
+        num_stacks = int(num_stacks)
+        show_hud_message("Current Anarchy", f"{num_stacks:,}", 15)
 
 save_file_name = HiddenSaveOption("workaround_value", 0)
 saved_anarchy = HiddenSaveOption("anarchy", 0)
 def on_save():
-    num_stacks_attr, *_ = get_anarchy_attrs()
-    stacks = 0.0
-    if num_stacks_attr:
-        stacks, *_ = num_stacks_attr.GetValue(get_pc())
+    num_stacks_attr, num_stacks, *_ = get_anarchy_attrs()
+    if num_stacks_attr and num_stacks is not None:
         save_file_name.value = get_pc().GetSaveGameNameFromId(0)
-        if stacks >= 0.0:
-            saved_anarchy.value = stacks
+        if num_stacks >= 0.0:
+            saved_anarchy.value = num_stacks
         else:
             saved_anarchy.value = 0
 
 def on_load():
     correct_save = (save_file_name.value == get_pc().GetSaveGameNameFromId(0))
-    num_stacks_attr, stack_cap_attr = get_anarchy_attrs()
-    if num_stacks_attr and stack_cap_attr:
-        if saved_anarchy.value == float('inf'):
+    num_stacks_attr, _, stack_cap_attr, stack_cap = get_anarchy_attrs()
+    if num_stacks_attr and stack_cap_attr and stack_cap is not None:
+        if saved_anarchy.value == inf:
             modified_anarchy = saved_anarchy.value
         else:
             modified_anarchy = round(saved_anarchy.value * anarchy_multiplier.value / 100)
             
         if stack_uncap.value and (correct_save):
             uncap_anarchy()
-            num_stacks_attr.SetAttributeBaseValue(get_pc(), modified_anarchy)
+            set_anarchy(modified_anarchy)
         elif not stack_uncap.value and (correct_save):
-            if modified_anarchy > stack_cap_attr.GetValue(get_pc())[0]:
-                num_stacks_attr.SetAttributeBaseValue(get_pc(), stack_cap_attr.GetValue(get_pc())[0])
+            if modified_anarchy > stack_cap:
+                set_anarchy(stack_cap)
             else:
-                num_stacks_attr.SetAttributeBaseValue(get_pc(), modified_anarchy)
+                set_anarchy(modified_anarchy)
 
-mod = mods_base.build_mod()
+def on_enable() -> None:
+    if stack_uncap.value:
+        uncap_anarchy()
+    if display_anarchy.value:
+        simple_math.enable()
+mod = mods_base.build_mod(
+    keybinds = [add_anarchy_key_pressed, subtract_anarchy_key_pressed, display_anarchy_key_pressed],
+    options = [anarchy_multiplier, anarchy_keys, stack_uncap, display_anarchy]
+    )
 register_save_options(mod)
